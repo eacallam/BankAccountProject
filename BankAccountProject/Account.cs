805985e070cb1747eeabe474383da98c53d1b157
eacallam <eacallam@yahoo.com>
2017-09-20 20:50:06 -0400

    First Commit

---
+++ BankAccountProject/Account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class Account
+    {
+ //This is the base class; must include:
+ //An account number
+ //Balance
+ //An account type(savings or checking)
+ //A way to view the current balance
+ //User must be able to deposit money into either account
+ //User must be able to withdraw money from either account
+
+ //This will be my abstract class
+
+
+
+    }
+}
---
+++ BankAccountProject/Instructions.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccountProject
+{
+    class Instructions
+    {
+/*  Overview
+
+You will be designing a console application to manage a client’s bank account.For now, the application will have only one client that is hard-coded into the system.
+The client has one checking account and one savings account.
+
+In the menu, after a user selects either View Balance, Deposit, or Withdraw, the program should then give the option of selecting which account.
+
+
+ After each transaction, the current balance should be displayed on the screen.
+ Your program should include:
+ 1 abstract method(at least)
+ 1 virtual method(at least)
+ 1 override method(at least)
+
+
+When a user launches the program the following menu should be displayed to the screen.
+
+1. View Client Information
+2. View Account Balance
+3. Deposit Funds
+4. Withdraw Funds
+5. Exit
+If the user wants to view account balance, deposit funds, or withdraw money, the user should be prompted to select which account.
+Be sure the user can continue making selections from the menu until all desired transactions have been completed.
+
+Stretch Tasks
+Right now this program is designed for one user. Giving the ability to enter a user name when the program begins would make the program useful for multiple users. 
+At this point do not worry about trying to add passwords.That is outside of the scope of what we know at this point.
+Letting the users log in with only their names is less secure, but we're focused on functionality right now.
+
+Hints
+Persistence is not required. Meaning each time you run the program all of the values can reset.
+You can choose any starting balance to begin the program.
+Think about what data type should be used for dealing with money. */
+    }
+}
